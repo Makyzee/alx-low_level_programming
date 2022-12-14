2a729fea53ec5951bda2f4e510495a76089aff03
Makyzee <favourchiamakaemenike2000@gmail.com>
2022-12-14 06:53:26 -0800

    _my message_

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 	printf("size of a char:%i byte(s)\n", sizeof(char));
 	printf("size of an int:%i byte(s)\n", sizeof(int));
 	printf("size of a long int:%i byte(s)\n", sizeof(long int));
-	printf("size of a long int:%i byte(s)\n", sizeof(long int));
+	printf("size of a long long int:%i byte(s)\n", sizeof(long int));
 	printf("size of a float:%i byte(s)\n", sizeof(float));
 	return (0);
 }
