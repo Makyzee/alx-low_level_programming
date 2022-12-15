ffd0c7db1430ae28f8ec4d424bbd12c3717e14e7
Makyzee <favourchiamakaemenike2000@gmail.com>
2022-12-15 06:16:46 -0800

    _my message_

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main -prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, m, 1;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (1 = 50; 1 < 58; 1++)
+			{
+				if (m < 1 && n < m)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(1);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
