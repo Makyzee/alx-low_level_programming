aaba3921feecbfad86a40bdaf40c6eca6be443db
Makyzee <favourchiamakaemenike2000@gmail.com>
2022-12-16 13:13:09 -0800

    _my message_

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+ *  _isupper - checks if letter is uppercase
+ *  @c: receives the character
+ *
+ *  Return: 1 if letter is uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c <= 'Z' && c >= 'A')
+	{
+		return (1);
+	}
+	else
+
+	return (0);
+}
