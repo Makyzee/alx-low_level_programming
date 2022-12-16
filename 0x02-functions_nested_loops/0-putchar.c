df83943eee0e2006154645f3cf81a6ad49e2b61f
Makyzee <favourchiamakaemenike2000@gmail.com>
2022-12-16 05:53:25 -0800

    _my message_

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,16 @@
 #include "main.h"
-/**
- * main - This code prints string _putchar
- * Return: 0 on Success
- */
 
 int main(void)
 {
-char c[] = "_putchar";
-int i;
-for (i = 0 ; i < 8 ; i++)
-{
-_putchar(c[i]);
-}
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
