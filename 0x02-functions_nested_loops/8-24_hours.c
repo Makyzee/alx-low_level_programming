3a12b3be4d2a05d267a1e9a09587fdf7b2e49c5b
Makyzee <favourchiamakaemenike2000@gmail.com>
2022-12-17 08:45:19 -0800

    _my message_

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - prints the minutes and seconds of time
+ *
+ */
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 23; x++)
+	{
+		for (y = 0; y <= 59; y++)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
