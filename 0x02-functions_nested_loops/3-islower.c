4ee829419bf637725293a44f4d55f7d3a1a45f24
Makyzee <favourchiamakaemenike2000@gmail.com>
2022-12-17 07:18:34 -0800

    _my message_

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+/**
+ * _islower - checks for lowercase character
+ * @c: receives the character
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
