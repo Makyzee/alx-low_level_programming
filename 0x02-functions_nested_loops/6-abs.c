552e4c2d6023e6325699211db1c13fdcda2b5d25
Makyzee <favourchiamakaemenike2000@gmail.com>
2022-12-17 08:13:57 -0800

    _my message_

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+ * _abs - gives absolute value
+ * @x: receives the negative integer
+ *
+ * Return: - of integer
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	return (-x);
+}
