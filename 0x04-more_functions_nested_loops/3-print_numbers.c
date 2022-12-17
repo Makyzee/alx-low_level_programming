b0eafd468b2ae6c72e3d7c5a9a2fa1d76af364df
Makyzee <favourchiamakaemenike2000@gmail.com>
2022-12-16 20:06:09 -0800

    _my message_

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9 with a new line
+ *
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
