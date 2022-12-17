922ccc40cd9a6265d6e7dde2200124d00ef759cf
Makyzee <favourchiamakaemenike2000@gmail.com>
2022-12-17 09:11:08 -0800

    _my message_

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - receives two integer and add them
+ * @x: receives first integer
+ * @y: receives second integer
+ *
+ * Return: the addition of the two integers
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
