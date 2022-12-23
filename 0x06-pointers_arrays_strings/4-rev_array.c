5beee25c280291eae9e6aa4bc28a3b01c8de8fb8
Makyzee <favourchiamakaemenike2000@gmail.com>
2022-12-22 23:59:55 -0800

    _my message_

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * reverse_array - reverses the contents of an array
+ * @a: receives the array
+ * @n: receives the element of the array a
+ */
+void reverse_array(int *a, int n)
+{
+	int temp, s, e;
+
+	s = 0;
+	e = n - 1;
+	/**
+	 * set value in array a in temp
+	 * then place the last array in
+	 * the first array then place
+	 * value in temp to last array
+	 */
+	while (s < e)
+	{
+		temp = a[s];
+		a[s] = a[e];
+		a[e] = temp;
+		s++;
+		e--;
+	}
+}
